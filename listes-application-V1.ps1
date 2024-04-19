5ae89640e13342a100320359ecadb8d22fa4fd2a
Malo <126970037+Malaudos35@users.noreply.github.com>
2024-04-19 22:37:43 +0200

    Add files via upload
    listes les app non natives sur windows dans un fichier txt

---
+++ listes-application-V1.ps1
@@ -0,0 +1,7 @@
+﻿# Récupérer la liste des applications qui ne sont pas des applications natives de Windows
+Get-WmiObject -Class Win32_Product | Where-Object { $_.Vendor -notlike "Microsoft Corporation" } | Select-Object -Property Name | Out-File -FilePath "$env:USERPROFILE\Downloads\listes-app.txt"
+
+
+# Get-ExecutionPolicy # avoir la politique d execution
+# Set-ExecutionPolicy Unrestricted # authorise l execution les scripts
+# Set-ExecutionPolicy Restricted   # refuse l execution des scripts
