fbe1efe9354080ab3f7831190c7aa3ed6124ae11
Malo <126970037+Malaudos35@users.noreply.github.com>
2024-05-25 00:38:57 +0200

    Update ssh.sh

--- bash/ssh.sh
+++ bash/ssh.sh
@@ -1,13 +1,201 @@
+root=no
+fail=3
+port=22
+# Fonction pour afficher des messages avec des couleurs
+info() {
+    echo -e "\e[32m[INFO] $1\e[0m"
+}
 
-sudo apt update 
-sudo apt upgrade
+error() {
+    echo -e "\e[31m[ERROR] $1\e[0m"
+}
+
+aide() {
+    echo -e "\e[33m[aide] $1\e[0m"
+}
+help(){
+    echo "option :"
+    echo "-r pour acctiver la connexion en root"
+    echo "-f <valeur> nombre de tentatives de connexions authorisé"
+    echo "-p <port> le port de connexion ssh"
+}
+
+info "installation ssh ..."
+
+# Analyser les options fournies par l'utilisateur
+while getopts ":rf:p:-:" option; do
+    case "${option}" in
+        r)
+            root=yes
+            ;;
+        f)
+            fail=${OPTARG}
+            ;;
+        p)
+            port=${OPTARG}
+            ;;
+        -)
+            case "${OPTARG}" in
+                help)
+                    show_help
+                    exit 0
+                    ;;
+                *)
+                    error "Option invalide: --${OPTARG}"
+                    show_help
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            error "Option invalide: -${OPTARG}"
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+info $root
+info $fail
+
+# sudo apt update 
+# sudo apt upgrade
 sudo apt install openssh-server -y
 
-sudo systemctl status ssh
 
 sudo apt-get install ufw -y
 sudo ufw allow ssh
 sudo systemctl enable --now ssh
 
+
+echo "
 #sudo nano /etc/ssh/sshd_config   puis ajouter    # permet de se conncter en root
-#PermitRootLogin yes
+PermitRootLogin $root
+
+# This is the sshd server system-wide configuration file.  See
+# sshd_config(5) for more information.
+
+# This sshd was compiled with PATH=/usr/local/bin:/usr/bin:/bin:/usr/games
+
+# The strategy used for options in the default sshd_config shipped with
+# OpenSSH is to specify options with their default value where
+# possible, but leave them commented.  Uncommented options override the
+# default value.
+
+Include /etc/ssh/sshd_config.d/*.conf
+
+Port $port
+#AddressFamily any
+#ListenAddress 0.0.0.0
+#ListenAddress ::
+
+#HostKey /etc/ssh/ssh_host_rsa_key
+#HostKey /etc/ssh/ssh_host_ecdsa_key
+#HostKey /etc/ssh/ssh_host_ed25519_key
+
+# Ciphers and keying
+#RekeyLimit default none
+
+# Logging
+#SyslogFacility AUTH
+#LogLevel INFO
+
+# Authentication:
+
+#LoginGraceTime 2m
+#PermitRootLogin prohibit-password
+#StrictModes yes
+MaxAuthTries $fail
+#MaxSessions 10
+
+#PubkeyAuthentication yes
+
+# Expect .ssh/authorized_keys2 to be disregarded by default in future.
+#AuthorizedKeysFile     .ssh/authorized_keys .ssh/authorized_keys2
+
+#AuthorizedPrincipalsFile none
+
+#AuthorizedKeysCommand none
+#AuthorizedKeysCommandUser nobody
+
+# For this to work you will also need host keys in /etc/ssh/ssh_known_hosts
+#HostbasedAuthentication no
+# Change to yes if you don't trust ~/.ssh/known_hosts for
+# HostbasedAuthentication
+#IgnoreUserKnownHosts no
+# Don't read the user's ~/.rhosts and ~/.shosts files
+#IgnoreRhosts yes
+
+# To disable tunneled clear text passwords, change to no here!
+#PasswordAuthentication yes
+#PermitEmptyPasswords no
+
+# Change to yes to enable challenge-response passwords (beware issues with
+# some PAM modules and threads)
+KbdInteractiveAuthentication no
+
+# Kerberos options
+#KerberosAuthentication no
+#KerberosOrLocalPasswd yes
+#KerberosTicketCleanup yes
+#KerberosGetAFSToken no
+
+# GSSAPI options
+#GSSAPIAuthentication no
+#GSSAPICleanupCredentials yes
+#GSSAPIStrictAcceptorCheck yes
+#GSSAPIKeyExchange no
+
+# Set this to 'yes' to enable PAM authentication, account processing,
+# and session processing. If this is enabled, PAM authentication will
+# be allowed through the KbdInteractiveAuthentication and
+# PasswordAuthentication.  Depending on your PAM configuration,
+# PAM authentication via KbdInteractiveAuthentication may bypass
+# the setting of "PermitRootLogin prohibit-password".
+# If you just want the PAM account and session checks to run without
+# PAM authentication, then enable this but set PasswordAuthentication
+# and KbdInteractiveAuthentication to 'no'.
+UsePAM yes
+
+#AllowAgentForwarding yes
+#AllowTcpForwarding yes
+#GatewayPorts no
+X11Forwarding yes
+#X11DisplayOffset 10
+#X11UseLocalhost yes
+#PermitTTY yes
+PrintMotd no
+#PrintLastLog yes
+#TCPKeepAlive yes
+#PermitUserEnvironment no
+#Compression delayed
+#ClientAliveInterval 0
+#ClientAliveCountMax 3
+#UseDNS no
+#PidFile /run/sshd.pid
+#MaxStartups 10:30:100
+#PermitTunnel no
+#ChrootDirectory none
+#VersionAddendum none
+
+# no default banner path
+#Banner none
+
+# Allow client to pass locale environment variables
+AcceptEnv LANG LC_*
+
+# override default of no subsystems
+# Subsystem       sftp    /usr/lib/openssh/sftp-server
+
+# Example of overriding settings on a per-user basis
+#Match User anoncvs
+#       X11Forwarding no
+#       AllowTcpForwarding no
+#       PermitTTY no
+#       ForceCommand cvs server " > "/etc/ssh/sshd_config"
+
+sudo systemctl restart ssh
+
+sudo systemctl status ssh
