acdd12bb1c0eea269e44820123331feb27ddd79c
Malo <126970037+Malaudos35@users.noreply.github.com>
2024-05-30 14:34:56 +0200

    Update local.sh

--- cours/local.sh
+++ cours/local.sh
@@ -5,8 +5,9 @@ nameserver 127.0.0.1
 
 nameserver 10.200.24.250
 nameserver 8.8.8.8
-" > "/etc/resolv.conf"
+" >> "/etc/resolv.conf"
 # nameserver 192.168.1.254
 interface=$(ip route get 1 | awk '{print $5}')
 
 ip addr add 10.200.24.250/24 dev $interface
+ip addr add 10.200.24.252/24 dev $interface
