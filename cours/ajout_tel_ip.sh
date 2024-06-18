37096ce49fe1b5e8b54e60b915a9623d284016a3
Malo <126970037+Malaudos35@users.noreply.github.com>
2024-06-18 10:42:21 +0200

    Create ajout_tel_ip.sh

---
+++ cours/ajout_tel_ip.sh
@@ -0,0 +1,17 @@
+if [ -z "$1" ]; then
+  echo "Usage: $0 <argument>"
+  exit 1
+fi
+echo "
+[$1] 
+
+Usename = $1 
+
+Secret = bonjour 
+
+Host = dynamic 
+
+Type = friend 
+
+Context = local 
+" >> /etc/asterisk/sip.conf
