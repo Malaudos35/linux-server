1bf3062c0680015a712720348b201e43311fc5f0
Malo <126970037+Malaudos35@users.noreply.github.com>
2024-07-20 00:12:33 +0200

    Create server_vpn_install.sh

---
+++ bash/server_vpn_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install curl -y
+mkdir -p /etc/vpn
+cd /etc/vpn
+sudo curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
+sudo chmod +x openvpn-install.sh
+sudo ./openvpn-install.sh
